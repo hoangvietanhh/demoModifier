8fd0699b12b0dc297ebdc214ddfcb2724709aeea
Việt Anh Hoàng <hoangvietanh1797@gmail.com>
2020-10-09 14:05:07 +0700

    add file

---
+++ src/mypack/Simple.java
@@ -0,0 +1,15 @@
+package mypack;
+import pack.*;
+public class Simple {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.msg();
+//        A obj = new A();
+//        obj.msg();
+    }
+//    public static void main(String[] args) {
+//        A obj = new A();
+//        System.out.println(obj.data);
+//        obj.msg();
+//    }
+}
---
+++ src/pack/A.java
@@ -0,0 +1,10 @@
+package pack;
+public class A {
+    public void msg(){
+        System.out.println("hello");
+    }
+//        public int data = 40;
+//        private void msg(){
+//            System.out.println("Hello Java");
+//        }
+}
